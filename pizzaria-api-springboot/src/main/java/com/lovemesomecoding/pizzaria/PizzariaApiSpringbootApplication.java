5fe67e8847c0f77a17c99b4c8576a37d76ce83cb
Folau Kaveinga <folaukaveinga@gmail.com>
2021-01-16 02:56:30 -0700

    added pizzaria springboot

---
+++ pizzaria-api-springboot/src/main/java/com/lovemesomecoding/pizzaria/PizzariaApiSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.lovemesomecoding.pizzaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzariaApiSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzariaApiSpringbootApplication.class, args);
+	}
+
+}
---
+++ pizzaria-api-springboot/src/test/java/com/lovemesomecoding/pizzaria/PizzariaApiSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lovemesomecoding.pizzaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzariaApiSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
